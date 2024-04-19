13353b490927edff1da1cee09c7dcd74ed491d50
rafatspn <rafatspn@gmail.com>
2024-04-18 22:36:18 -0500

    pdf query added

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+import os
+import torch
+import weaviate
 import warnings
 import argparse
-import weaviate
-#from langchain_community.vectorstores import Weaviate 
-#from weaviate.classes.config import Property, DataType
-from langchain_community.document_loaders import PyPDFLoader   
-from langchain_text_splitters import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores.weaviate import Weaviate
-from sentence_transformers import SentenceTransformer
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 
-client=None
-model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+load_dotenv()
 
+def create_db_instance():
+    DB_URL = os.getenv("DB_URL")
+    client = weaviate.Client(url=DB_URL)
+    print("Connected to database")
+    return client
 
 def read_pdf(file_path):
     pdf_content = PyPDFLoader(file_path).load_and_split()
@@ -24,33 +32,76 @@ def chunk_pdf(pdf_content):
     chunks = text_splitter.split_documents(pdf_content)
     return chunks
 
-def gen_embeddings(chunks, source):
-    vs = Weaviate.from_documents(documents=chunks, embedding=model, client=client)
-    print("Indexing successfully")
-    return vs
+def get_emb_mode():
+    emb_model = os.getenv("EMB_MODEL")
+    model = HuggingFaceEmbeddings(model_name=emb_model)
+    return model
 
+def insert(client, chunks, emb_model):
+    response = Weaviate.from_documents(documents=chunks, embedding=emb_model,client=client)
+    print("Inserted Successfully")
+    return response
 
-def start():
+def initialize_model(model_name):
+    token = os.getenv("HF_TOKEN")
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, 
+        device_map="auto",  
+        torch_dtype=torch.bfloat16,
+        token=token,
+    )
+    return model
+
+def initialize_tokenizer(model_name):
+    tokenizer = AutoTokenizer.from_pretrained(model_name, return_token_type_ids=False)
+    tokenizer.bos_token_id = 1 
+    return tokenizer
+
+def initialize_llm(model, tokenizer):
+    llm = HuggingFacePipeline(pipeline=pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        device_map="auto",
+        use_cache=True,
+        max_length = 2048,
+        do_sample=True,
+        top_k=3,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.eos_token_id
+    ))
+
+    return llm
+
+try:
     warnings.filterwarnings("ignore")
     parser = argparse.ArgumentParser(description="Read a PDF file.")
     parser.add_argument("--pdf_file", type=str, required=True, help="Path to the PDF file")
+    parser.add_argument("--query", type=str, required=True, help="Query to the model")
     args = parser.parse_args()
 
-    pdf_content = read_pdf(args.pdf_file)
-    chunks = chunk_pdf(pdf_content)
-    vs = gen_embeddings(chunks, args.pdf_file)
-    #ret = vs.as_retriever(search_type="similarity")
-    #q = "What is attention?"
-    #response = ret.invoke(q)
-    #print(response)
+    pdf_file = args.pdf_file
+    query = args.query
 
-try: 
-    client = weaviate.Client(
-        url="https://579-project-24szk15n.weaviate.network",
-        auth_client_secret=weaviate.AuthApiKey("AoWIMOVxQzgfyHaVIAJMwXsg6mcTuxzeVoUr")
+    pdf_contents = read_pdf(pdf_file)
+    chunks = chunk_pdf(pdf_contents)
+    emb_model = get_emb_mode()
+
+    client = create_db_instance()
+    storage = insert(client, chunks, emb_model)
+
+    model_name = os.getenv("MODEL")
+    model = initialize_model(model_name)
+    tokenizer = initialize_tokenizer(model_name)
+
+    llm = initialize_llm(model, tokenizer)
+
+    retrieve_answer = RetrievalQA.from_chain_type(
+        llm=llm, chain_type="stuff", retriever=storage.as_retriever()
     )
-    print("Connected to database successfully")
-    start()
+
+    resp = retrieve_answer.run(query)
+    print(resp)
 except Exception as e:
-    print("Failed to connect to database!")
     print(e)
