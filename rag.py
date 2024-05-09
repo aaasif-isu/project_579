19f6576221240a8310965c0a41f5a2bdd6e149b1
rafatspn <rafatspn@gmail.com>
2024-05-09 13:22:51 -0500

    removed extra code

--- rag.py
+++ rag.py
@@ -26,9 +26,8 @@ import openai
 load_dotenv()
 
 client = None
-Database_url="https://579-project-upeahmau.weaviate.network"
+Database_url=os.getenv("DB_URL")
 openai_key= os.getenv("OPENAI_KEY")
-#storage = None
 
 def Upload_PDF(PDF_Files: List[BytesFile],
     ) -> Markdown:
@@ -54,41 +53,8 @@ def Upload_PDF(PDF_Files: List[BytesFile],
             
 
     def get_emb_mode():
-        #model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
         model = OpenAIEmbeddings(api_key=openai_key)
         return model
-    
-    # def initialize_model(model_name):
-    # # token = os.getenv("HF_TOKEN")
-    #     model = AutoModelForCausalLM.from_pretrained(
-    #         model_name, 
-    #         device_map="auto",  
-    #         torch_dtype=torch.bfloat16,
-    #     )
-    #     #model = OpenAI(api_key=openai_key)
-    #     return model
-    
-    # def initialize_tokenizer(model_name):
-    #     tokenizer = AutoTokenizer.from_pretrained(model_name, return_token_type_ids=False)
-    #     tokenizer.bos_token_id = 1 
-    #     return tokenizer
-    
-    # def initialize_llm(model, tokenizer):
-    #     llm = HuggingFacePipeline(pipeline=pipeline(
-    #         "text-generation",
-    #         model=model,
-    #         tokenizer=tokenizer,
-    #         device_map="auto",
-    #         use_cache=True,
-    #         max_length = 2048,
-    #         do_sample=True,
-    #         top_k=3,
-    #         num_return_sequences=1,
-    #         eos_token_id=tokenizer.eos_token_id,
-    #         pad_token_id=tokenizer.eos_token_id
-    #     ))
-
-    #     return llm
     
     try:
         pdf_contents = read_pdf(PDF_Files)    
@@ -108,95 +74,16 @@ def Upload_PDF(PDF_Files: List[BytesFile],
 
         return f"{message}"
 
-        # model_name = "Writer/palmyra-small"
-        # # llm = OpenAI(temperature=0.7, model_name=model_name)
-        # # memory = ConversationBufferMemory(
-        # # memory_key='chat_history', return_messages=True)
-        # # conversation_chain = ConversationalRetrievalChain.from_llm(
-        # #     llm=llm,
-        # #     chain_type="stuff",
-        # #     retriever=storage.as_retriever(),
-        # #     memory=memory
-        # # )
-
-        # model = initialize_model(model_name)
-
-        # tokenizer = initialize_tokenizer(model_name)
-
-        # llm = initialize_llm(model, tokenizer)
-
-        # retrieve_answer = RetrievalQA.from_chain_type(
-        #     llm=llm, chain_type="stuff", retriever=storage.as_retriever()
-        # )
-
-        # resp = retrieve_answer.run(Query)
-
-        # # result = conversation_chain({"question": Query})
-        # # answer = result["answer"]
-
-        # return f"{resp}"
-
     except Exception as e:
         return f"{e}"
 
 def Query(Query : str) -> Markdown:
-    # def initialize_model(model_name):
-    #     # token = os.getenv("HF_TOKEN")
-    #     # model = AutoModelForCausalLM.from_pretrained(
-    #     #     model_name, 
-    #     #     device_map="auto",  
-    #     #     torch_dtype=torch.bfloat16,
-    #     # )
-    #     model = OpenAI(api_key=openai_key)
-    #     return model
-    
-    # def initialize_tokenizer(model_name):
-    #     tokenizer = AutoTokenizer.from_pretrained(model_name, return_token_type_ids=False)
-    #     tokenizer.bos_token_id = 1 
-    #     return tokenizer
-    
-    # def initialize_llm(model, tokenizer):
-    #     llm = HuggingFacePipeline(pipeline=pipeline(
-    #         "text-generation",
-    #         model=model,
-    #         tokenizer=tokenizer,
-    #         device_map="auto",
-    #         use_cache=True,
-    #         max_length = 2048,
-    #         do_sample=True,
-    #         top_k=3,
-    #         num_return_sequences=1,
-    #         eos_token_id=tokenizer.eos_token_id,
-    #         pad_token_id=tokenizer.eos_token_id
-    #     ))
-
-    #     return llm
-
     model_name = "gpt-3.5-turbo-instruct"
     llm = OpenAI(api_key=openai_key,temperature=0.7, model_name=model_name)
-    # memory = ConversationBufferMemory(
-    # memory_key='chat_history', return_messages=True)
     chain = RetrievalQA.from_chain_type(
         llm=llm, chain_type="stuff", retriever=storage.as_retriever(search_type="similarity",search_kwargs={'k':3})
     )
 
     answer = chain.invoke(Query)["result"]
-
-    # model = initialize_model(model_name)
-
-    # tokenizer = initialize_tokenizer(model_name)
-
-    # llm = initialize_llm(model, tokenizer)
-
-    # retrieve_answer = RetrievalQA.from_chain_type(
-    #     llm=llm, chain_type="stuff", retriever=storage.as_retriever()
-    # )
-
-    # resp = retrieve_answer.run(Query)
-
-    #result = conversation_chain({"question": Query})
-    #answer = result["answer"]
-
-    #print(result)
 
     return f"{answer}"
